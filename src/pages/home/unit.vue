01d43f2551e3164452e236dd4cef45e1fa65ecdf
Herman Leus <glal@ukr.net>
2017-04-16 13:20:53 +0300

    Added fonts

--- src/pages/home/unit.vue
+++ src/pages/home/unit.vue
@@ -1,7 +1,5 @@
 <template src="./template.html"></template>
 
 <script src="./component.js">
-
 </script>
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped src="./style.css"></style>
+
